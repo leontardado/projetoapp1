cfcbc102bd984a56b8f6a5eae3d877ca81ea4ff5
mateussantos <mateus.santos@ifsuldeminas.edu.br>
2026-03-09 14:52:58 -0300

    ProjetoBase

---
+++ app/src/main/java/com/example/projetointegradomemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.projetointegradomemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btn){
+            Intent i = new Intent(this, Tela02.class);
+            startActivity(i);
+        }
+    }
+}
